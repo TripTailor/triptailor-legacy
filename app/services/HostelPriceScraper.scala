716956cdb0ef68b7d69f71a6d6d235d5fc3c87ee
Samuel Heaney <sheaney@gmail.com>
2015-09-12 00:11:06 -0500

    Retries hostel price scraper within a specified timeout

--- app/services/HostelPriceScraper.scala
+++ app/services/HostelPriceScraper.scala
@@ -1,13 +1,17 @@
 package services
 
-import javax.inject.Inject
+import java.util.concurrent.TimeoutException
+import javax.inject.{Inject, Singleton}
+
+import org.jsoup.Jsoup
 import play.api.Configuration
 import play.api.Play.current
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.libs.json.JsValue
 import play.api.libs.ws.WS
+
 import scala.concurrent.Future
-import org.jsoup.Jsoup
+import scala.util.control.NonFatal
 
 sealed trait DormType
 case object PublicDorm extends DormType
@@ -17,14 +21,19 @@ case object UnknownDorm extends DormType
 case class PricingInfo(price: Option[BigDecimal], dormType: DormType, currency: Option[String])
 case class HostelPricingInfo(id: String, pricingInfo: PricingInfo)
 
+@Singleton
 class HostelPriceScraper @Inject() (config: Configuration) {
+  import HostelPriceScraper._
+
   val timeout = config.getMilliseconds("scraper.pricingInfo.timeout").get
-  
+
   def retrievePricingInfo(city: String, country: String, dateFrom: String, dateTo: String): Future[Seq[HostelPricingInfo]] =
-    tokenRequest("city" -> city, "country" -> country, "date_from" -> dateFrom, "date_to" -> dateTo) flatMap { token =>
-      parsePricingInfo(token)
+    retryRequestWithin(timeout) {
+      tokenRequest("city" -> city, "country" -> country, "date_from" -> dateFrom, "date_to" -> dateTo) flatMap { token =>
+        parsePricingInfo(token)
+      }
     }
-  
+
   private def tokenRequest(queryParams: (String, String)*): Future[String] =
     hostelworldSearchRequest(queryParams: _*)
       .map(_.body).map(retrieveToken)
@@ -58,5 +67,24 @@ class HostelPriceScraper @Inject() (config: Configuration) {
     
   private def jsonRequest(token: String) =
     WS.url(s"http://www.hostelworld.com/static/js/1.32.14.4/properties-ajax-$token").withRequestTimeout(timeout).get()
+
+}
+
+object HostelPriceScraper {
+
+  def retryRequestWithin[A](timeout: Long)(request: => Future[A]): Future[A] = {
+    def retry(millis: Long): Future[A] = {
+      val start = System.currentTimeMillis()
+      request.recoverWith {
+        case NonFatal(_) =>
+          val remainingTime = millis - (System.currentTimeMillis() - start)
+          if (remainingTime > 0)
+            retry(remainingTime)
+          else Future.failed(new TimeoutException(s"Timed out after $timeout millis"))
+      }
+    }
+
+    retry(timeout)
+  }
 
 }
