6af4200f0c983e2004edc7cd3cedcdfea7b0cb2a
Samuel Heaney <sheaney@gmail.com>
2015-10-07 22:04:57 -0500

    Adds reviews data to classified hostel

--- app/models/ClassifiedHostel.scala
+++ app/models/ClassifiedHostel.scala
@@ -14,14 +14,15 @@ case class ClassifiedHostel(hostel: Hostel, rating: Double, orderedTags: Seq[Tag
 object ClassifiedHostel {
   implicit val classifiedHostelWrites = new Writes[ClassifiedHostel] {
     def writes(ch: ClassifiedHostel) = Json.obj(
-      "id"     -> ch.hostel.id,
-      "name"   -> ch.hostel.name,
+      "id"          -> ch.hostel.id,
+      "name"        -> ch.hostel.name,
       "description" -> ch.hostel.description.map(StringEscapeUtils.unescapeHtml4),
-      "price"  -> ch.hostel.price.map(_.setScale(2, RoundingMode.HALF_EVEN)),
-      "currency" -> ch.hostel.currency,
-      "images" -> new HostelImageUrlsBuilder(Play.current.configuration).hostelWorldUrls(ch.hostel),
-      "url"    -> ch.hostel.url,
-      "tags"   -> ch.orderedTags
+      "price"       -> ch.hostel.price.map(_.setScale(2, RoundingMode.HALF_EVEN)),
+      "currency"    -> ch.hostel.currency,
+      "images"      -> new HostelImageUrlsBuilder(Play.current.configuration).hostelWorldUrls(ch.hostel),
+      "url"         -> ch.hostel.url,
+      "tags"        -> ch.orderedTags,
+      "reviewsData" -> ch.hostel.reviewsData
     )
   }
 }
--- app/models/ReviewData.scala
+++ app/models/ReviewData.scala
@@ -1,6 +1,7 @@
 package models
 
 import org.joda.time.DateTime
+import play.api.libs.json.{Json, Writes}
 
 case class AttributePositions(name: String, positions: String)
 case class ReviewData(
@@ -12,3 +13,26 @@ case class ReviewData(
   gender: Option[String],
   age: Option[Int]
 )
+
+object AttributePositions {
+  implicit val attributePositionWrites = new Writes[AttributePositions] {
+    def writes(ap: AttributePositions) = Json.obj(
+      "tag"        -> ap.name,
+      "positions" -> ap.positions
+    )
+  }
+}
+
+object ReviewData {
+  implicit val reviewDataWrites = new Writes[ReviewData] {
+    def writes(rd: ReviewData) = Json.obj(
+      "tagPositions" -> Json.toJson(rd.attributePositions),
+      "text"         -> rd.text,
+      "year"         -> rd.year,
+      "reviewer"     -> rd.reviewer,
+      "city"         -> rd.city,
+      "gender"       -> rd.gender,
+      "age"          -> rd.age
+    )
+  }
+}
